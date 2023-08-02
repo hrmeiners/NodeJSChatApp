5ed38f4dc02314ccc3002b515169d34de7dd8886
Henry Meiners <henry.meiners@slu.edu>
2023-08-02 17:06:47 -0500

    new tutorial

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-var express = require('express');
-var app = express();
+//sets up express, socket.io
+//creates the HTTP server
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')('http');
+
+
+io.on('connection', (socket) => {
+    console.log('Connected');
+    
+    //emits message to ALL connected clients
+    //to emit to specific client instead use socket.emit()
+    socket.on('message', (data) => {
+        console.log('Your Message: ', data);
+        io.emit('message', data);
+    });
+
+});
+
+http.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
